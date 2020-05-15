0ea606d93b2e1bbe0066e821a926e9d8cc3c2de4
Fauzan Yohtae <fauzanlk@gmail.com>
2020-05-15 23:39:51 +0700

    Update api.php

--- starcomment/api.php
+++ starcomment/api.php
@@ -1,1 +1,3 @@
+//สร้าง controller ก่อน หรือใช้ controller ที่มีอยู่แล้ว
 Route::post('giveStar', 'API\PostRatingController@giveStar');
+Route::get('starScore/{id}', 'API\PostRatingController@starScore');
